592a581b09835d36da02dc068b7830403c60871c
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-06-15 18:29:13 +0200

    feat : service hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut request = [0; 1024];
+    stream.read(&mut request).unwrap();
+
+    let content = String::from("hello world");
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        content.len(),
+        content
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
