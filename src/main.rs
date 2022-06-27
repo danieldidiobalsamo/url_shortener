ca4f86888a78a46cfffe83ec047efb556d100c78
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-06-27 19:28:32 +0200

    feat: http://127.0.0.1:8080/decode/<short_url> redirects to original url

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{http, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, http, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 use url_shortener_algo;
 use url_shortener_redis_server::{self, RedisClient};
@@ -16,13 +16,29 @@ async fn shorten_url_request(info: web::Json<EncodeReqInfo>) -> impl Responder {
     let mut redis = RedisClient::new("127.0.0.1", "6379");
     url_shortener_redis_server::add_url(&mut redis, &short, &url);
 
-    HttpResponse::build(http::StatusCode::OK).body(short) // tmp
+    HttpResponse::build(http::StatusCode::OK).body(short)
+}
+
+#[get("/decode/{url}")]
+async fn retrieve_full_url(path: web::Path<String>) -> impl Responder {
+    let url = path.into_inner();
+
+    let mut redis = RedisClient::new("127.0.0.1", "6379");
+    let full = url_shortener_redis_server::get_full_url(&mut redis, &url);
+
+    HttpResponse::build(http::StatusCode::MOVED_PERMANENTLY)
+        .insert_header(("Location", full))
+        .body("redirecting...")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(shorten_url_request))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(shorten_url_request)
+            .service(retrieve_full_url)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
