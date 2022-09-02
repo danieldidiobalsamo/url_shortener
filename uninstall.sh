f7e4c3098bc4a15b59c38b27e60a533e50815b5e
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-09-02 13:51:55 +0200

    feat: uninstall script removes url shortener resolution in /etc/hosts

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,12 @@
 text="This script is going to remove url-shortener app, including its persistent volume.
 It won't modify cert-manager namespace.
 
+It also removes 'url-shortener-rust.com' resolution in /etc/hosts
+
 Are you sure to continue ?"
 
-if (whiptail --title "url-shortener uninstall" --yesno "$text" 12 60) then
+if (whiptail --title "url-shortener uninstall" --yesno "$text" 14 60) then
     helm uninstall url-shortener
+    echo "sudo sed -i '/url-shortener-rust.com/d' /etc/hosts"
+    sudo sed -i '/url-shortener-rust.com/d' /etc/hosts
 fi
