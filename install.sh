65e7802a662ff9caa9fffc777313e38e86794bf3
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-10-25 12:39:37 +0200

    refactor: rename k8s objects

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 echo -e 'Downloading cert-manager CRDs...\n'
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
 
-echo -e 'Setup url-shortener application...\n'
+echo -e 'Setup url-shortener application and wait for pods / ingress...\n'
 helm install url-shortener deployment/rust-url-shortener --wait
 echo -e 'Done.\n'
 
 # add ingress IP to /etc/hosts
-ip=`kubectl get ingress --field-selector metadata.name=app-ingress --namespace url-shortener -o custom-columns=:.status.loadBalancer.ingress[0].ip | tr -d '\n'`
+ip=`kubectl get ingress --field-selector metadata.name=url-shortener --namespace url-shortener -o custom-columns=:.status.loadBalancer.ingress[0].ip | tr -d '\n'`
 mapping="$ip    short.home"
 
 text="The following resolution has to be written in /etc/hosts
