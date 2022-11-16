bcb7784ea6024f1186feadd9f7014cd81668456e
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-11-16 19:38:42 +0100

    feat: let user choose application domain name

--- install.sh
+++ install.sh
@@ -3,13 +3,25 @@
 echo -e 'Downloading cert-manager CRDs...\n'
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
 
+# let user choose the application domain name
+text="
+Please choose url-shortener application domain name.
+
+Note: if a private domain name is provided, a 'Kubernetes Ingress Controller Fake Certificate' is issued. Otherwise let's encrypt issues one.
+"
+
+domainName=$(whiptail --title "url-shortener installation" --inputbox "$text" 16 60 short.home 3>&1 1>&2 2>&3)
+
+# deploy app
 echo -e 'Setup url-shortener application and wait for pods / ingress...\n'
-helm install url-shortener deployment/rust-url-shortener --wait
+helm install url-shortener deployment/rust-url-shortener \
+  --set applicationUrl=$domainName \
+  --wait
 echo -e 'Done.\n'
 
 # add ingress IP to /etc/hosts
 ip=`kubectl get ingress --field-selector metadata.name=url-shortener --namespace url-shortener -o custom-columns=:.status.loadBalancer.ingress[0].ip | tr -d '\n'`
-mapping="$ip    short.home"
+mapping="$ip    $domainName"
 
 text="The following resolution has to be written in /etc/hosts
 
@@ -25,9 +37,9 @@ if (whiptail --title "url-shortener installation" --yesno "$text" 16 60) then
   echo "$mapping" | cat - /etc/hosts > /tmp/hosts_tmp && sudo mv /tmp/hosts_tmp /etc/hosts
 
   echo -e '(10/10) Application is deployed !'
-  echo -e 'Open this link : https://short.home'
+  echo -e "Open this link : https://$domainName"
 else
   echo "Please manually paste this line in /etc/hosts:"
   echo -e "$mapping\n"
-  echo "Then open this link : https://short.home"
+  echo -e "Then Open this link : https://$domainName"
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# get app domain name (the user can choose a custom one using install script)
+domainName=`kubectl get ingress --field-selector metadata.name=url-shortener --namespace url-shortener -o custom-columns=:.spec.tls[0].hosts[0]`
+
 text="This script is going to remove url-shortener app, including its persistent volume.
 
-It also removes 'short.home' resolution in /etc/hosts
+It also removes url-shortener name resolution in /etc/hosts
 
 Are you sure to continue ?"
 
@@ -10,6 +13,8 @@ if (whiptail --title "url-shortener uninstall" --yesno "$text" 14 60) then
     echo "helm uninstall..."
     helm uninstall url-shortener --wait --timeout=120s
     
-    echo "sudo sed -i '/short.home/d' /etc/hosts"
-    sudo sed -i '/short.home/d' /etc/hosts
+    regex=$(printf "/%s/d" $domainName)
+    cmd="sudo sed -i "$regex" /etc/hosts"
+    echo $cmd
+    $cmd
 fi
