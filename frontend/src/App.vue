8f27a37d32b18821ab459774660103d9ff77adee
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-01-27 12:23:53 +0100

    feat: vuejs hello world

---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <Form msg="Hello World!" />
+</template>
---
+++ frontend/src/components/Form.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg || 'test'}}</h1>
+</template>
