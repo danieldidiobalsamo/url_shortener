1e19766ebfbc9b594a407a200305b307b812575a
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-02-20 18:12:03 +0100

    feat: general css sketch

--- frontend/src/components/UrlForm.vue
+++ frontend/src/components/UrlForm.vue
@@ -1,11 +1,17 @@
 <template>
-  <form @submit.prevent="shortUrl">
-    <input type="text" placeholder="URL to shorten..." v-model="urlToShorten">
-    <button>Shorten</button>
-  </form>
-
-  <input type="text" :value='shortenedUrl'>
-
+  <header>
+    hello header
+  </header>
+  <main>
+    <form @submit.prevent="shortUrl">
+        <input type="text" placeholder="URL to shorten..." v-model="urlToShorten">
+        <button>Shorten</button>
+    </form>
+    <input type="text" :value='shortenedUrl'>
+  </main>
+  <footer>
+    hello footer
+  </footer>
 </template>
 
 <script>
@@ -58,3 +64,33 @@ function isUrl(input){
   return true;
 }
 </script>
+
+<style type="text/css">
+  *{
+    font-size: 20px;
+  }
+
+  body{
+    margin: 0;
+  }
+
+  main{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+    height: 80vh;
+    width: 100vw;
+  }
+
+  header{
+    height: 10vh;
+    width: 100vw;
+  }
+
+  footer{
+    height: 10vh;
+    width: 100vw;
+  }
+</style>
