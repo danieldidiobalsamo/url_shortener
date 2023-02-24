2d9869b2fccbdaf94fb065f5e5d71cf43d2a5e97
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-02-24 11:56:09 +0100

    feat: page main theme

--- frontend/src/components/UrlForm.vue
+++ frontend/src/components/UrlForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="main-item">
-    <form @submit.prevent="shortUrl">
-        <input type="text" placeholder="URL to shorten..." v-model="urlToShorten">
-        <button>Shorten</button>
+    <form id="url-form" @submit.prevent="shortUrl">
+      <input type="text" placeholder="URL to shorten..." v-model="urlToShorten">
+      <button>Shorten</button>
     </form>
     <input type="text" :value='shortenedUrl'>
   </div>
@@ -64,9 +64,45 @@ function isUrl(input){
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
+
     align-content: center;
     justify-content: center;
+    row-gap: 5%;
+
     height: 84vh;
     width: 100vw;
+  }
+
+  #url-form{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    column-gap: 5em;
+  }
+
+  #main-item input, button{
+    background: #2480FF;
+    color: white;
+    border-radius: 5px;
+    border-color: #1578ff;
+  }
+
+  #main-item input{
+    width: 35em;
+    height: 2em;
+  }
+
+  #main-item input::placeholder{
+    color: white;
+    opacity: 0.7;
+  }
+
+  #main-item button{
+    width: 10em;
+    height: 2em;
+  }
+
+  #main-item button:hover{
+      background-color: #1D66CC;
   }
 </style>
