7c160000cc0f4bff02b9d0bc5c350148eef64b90
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-04-18 12:06:09 +0200

    feat: uninstall redis statefulset properly

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-text="This script is going to remove url-shortener app, including its persistent volume.
+text="This script is going to remove url-shortener app and its redis-cluster, including persistent datas.
 
 It also removes url-shortener name resolution in /etc/hosts
 
 Are you sure to continue ?"
 
 if (whiptail --title "url-shortener uninstall" --yesno "$text" 14 60) then
-    echo "helm uninstall..."
-    helm uninstall url-shortener --wait --timeout=120s
+
+    echo "scaling redis statefulset to 0..."
+    kubectl scale sts redis-sts --replicas=0 -n url-shortener-redis
+    echo "waiting for all redis pods to terminate..."
+    kubectl wait --for=jsonpath='{.status.availableReplicas}'=0 sts/redis-sts -n url-shortener-redis
+
+    helm uninstall url-shortener-redis -n url-shortener-redis
+    kubectl delete ns url-shortener-redis
+
+    helm uninstall url-shortener -n url-shortener
+    kubectl delete ns url-shortener
+
     
     regex=$(printf "/%s/d" "short.home")
     cmd="sudo sed -i "$regex" /etc/hosts"
