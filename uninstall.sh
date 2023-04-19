34e726f76a4a3341f5de1d52581909698c0d8064
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-04-19 18:06:57 +0200

    fix: uninstall order

--- uninstall.sh
+++ uninstall.sh
@@ -8,17 +8,19 @@ Are you sure to continue ?"
 
 if (whiptail --title "url-shortener uninstall" --yesno "$text" 14 60) then
 
+    echo "remove url-shortener..."
+
+    helm uninstall url-shortener -n url-shortener
+    kubectl delete ns url-shortener
+
     echo "scaling redis statefulset to 0..."
     kubectl scale sts redis-sts --replicas=0 -n url-shortener-redis
     echo "waiting for all redis pods to terminate..."
     kubectl wait --for=jsonpath='{.status.availableReplicas}'=0 sts/redis-sts -n url-shortener-redis
 
+    echo "remove url-shortener-redis..."
     helm uninstall url-shortener-redis -n url-shortener-redis
     kubectl delete ns url-shortener-redis
-
-    helm uninstall url-shortener -n url-shortener
-    kubectl delete ns url-shortener
-
     
     regex=$(printf "/%s/d" "short.home")
     cmd="sudo sed -i "$regex" /etc/hosts"
